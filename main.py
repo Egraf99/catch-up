1da580d5c9076d0c14e94fb17e39781a0efc5550
Egraf99 <77966173+Egraf99@users.noreply.github.com>
2021-03-01 15:27:38 +0300

    collision balls

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from math import *
 WIDTH = 800
 HEIGHT = 800
 CIRCLE_SIZE = 10
-figures = []
-max_figures = 25
+
 
 
 class App:
@@ -18,11 +17,12 @@ class App:
         c.focus_set()
         c.bind('<Button-3>', find_coord)
         c.bind('<Button-1>', go_all)
-        check_figure()
         root.mainloop()
 
 
 class Circle:
+    figures = []
+    max_figures = 25
 
     def __init__(self, x, y, size, speed_x, speed_y):
         self.size = size
@@ -34,6 +34,7 @@ class Circle:
         self.item = c.create_oval(x - CIRCLE_SIZE * self.size, y - CIRCLE_SIZE * self.size,
                                   x + CIRCLE_SIZE * self.size, y + CIRCLE_SIZE * self.size,
                                   fill='green')
+        self.check_collision()
 
     def _find_coefficient(self):
         x0, y0, x1, y1 = c.coords(self.item)
@@ -68,20 +69,44 @@ class Circle:
             self.x_finish, self.y_finish = event.x, event.y
             self.kx, self.ky, = self._find_coefficient()
 
-        if c.coords(self.item)[3] > HEIGHT or c.coords(self.item)[1] < 0:
-            self.kx = -self.kx
-        if c.coords(self.item)[0] < 0 or c.coords(self.item)[2] > WIDTH:
-            self.ky = -self.ky
+        try:
+            if c.coords(self.item)[3] > HEIGHT or c.coords(self.item)[1] < 0:
+                self.kx = -self.kx
+            if c.coords(self.item)[0] < 0 or c.coords(self.item)[2] > WIDTH:
+                self.ky = -self.ky
 
-        c.move(self.item, self.ky, self.kx)
+            c.move(self.item, self.ky, self.kx)
+        except Exception:
+            pass
 
         root.after(60, self.move)
 
     def change_size(self, x, y):
         self.size += 1
+        c.delete(self.item)
         self.item = c.create_oval(x - CIRCLE_SIZE * self.size, y - CIRCLE_SIZE * self.size,
                                   x + CIRCLE_SIZE * self.size, y + CIRCLE_SIZE * self.size,
                                   fill='green')
+
+    def delete(self):
+        try:
+            c.delete(self.item)
+            self.figures.remove(self)
+        except ValueError:
+            pass
+
+    def check_collision(self):
+        try:
+            x0, y0, x1, y1 = c.coords(self.item)
+            items = c.find_overlapping(x0, y0, x1, y1)
+            for item in filter(lambda i: i is not self.item, items):
+                for fig in Circle.figures:
+                    if fig.item == item:
+                        fig.delete()
+                        self.change_size(x0, y0)
+            root.after(1, self.check_collision)
+        except Exception:
+            pass
 
 
 def find_coord(event):
@@ -89,30 +114,14 @@ def find_coord(event):
 
 
 def made_figure(x, y, size=1, speed_x=None, speed_y=None):
-    if len(figures) <= max_figures:
+    if len(Circle.figures) <= Circle.max_figures:
         figure = Circle(x, y, size, speed_x, speed_y)
-        figures.append(figure)
+        Circle.figures.append(figure)
 
 
 def go_all(event):
-    check_figure()
-    for f in figures:
+    for f in Circle.figures:
         f.move(event)
-
-
-def check_figure():
-    for f in figures:
-        x0, y0, x1, y1 = c.coords(f.item)
-        items = c.find_overlapping(x0, y0, x1, y1)
-        for j in filter(lambda i: i is not f.item, items):
-            print(items)
-            c.delete(f.item)
-            figures.remove(f)
-            print(items)
-            x0a, y0a, x1a, y1a = c.coords(figures[j-1].item)
-            print([x0a, y0a, x1a, y1a])
-            figures[j - 1].change_size((x1a + x0a)/2, (y1a + y0a)/2)
-    root.after(1, check_figure)
 
 
 if __name__ == '__main__':
